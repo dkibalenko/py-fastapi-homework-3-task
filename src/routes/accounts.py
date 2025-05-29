69e547c506a1e2ef2ba3d92d0b42d511315b66d4
Denys Kibalenko <dennieking87@gmail.com>
2025-05-29 18:27:48 +0200

    Feat: add `register` endpoint to src/routes/accounts.py for registering a new user, assinging them to default `UserGroupEnum.USER` group & creating an activation token

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -19,7 +19,47 @@ from database import (
 )
 from exceptions import BaseSecurityError
 from security.interfaces import JWTAuthManagerInterface
+from schemas import UserRegistrationRequestSchema, UserRegistrationResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=status.HTTP_201_CREATED
+)
+async def register(
+    user_data: UserRegistrationRequestSchema,
+    db: AsyncSession = Depends(get_db)
+):
+    try:
+        query = select(UserModel).where(UserModel.email == user_data.email)
+        result = await db.execute(query)
+        db_user = result.scalar_one_or_none()
+
+        if db_user:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"A user with this email {user_data.email} already exists."
+            )
+
+        db_user = UserModel.create(
+            email=user_data.email,
+            raw_password=user_data.password,
+            group_id=1
+        )
+        db.add(db_user)
+        await db.flush()
+
+        activation_token = ActivationTokenModel(user_id=db_user.id)
+        db.add(activation_token)
+        await db.commit()
+        await db.refresh(db_user)
+
+        return db_user
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation."
+        )
