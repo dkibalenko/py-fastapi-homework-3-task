336bc4d78af481f6c62cd94a8852456287e43c2f
Denys Kibalenko <dennieking87@gmail.com>
2025-06-03 15:50:35 +0200

    Feat: add `UserLoginRequestSchema`, `UserLoginResponseSchema`, `TokenRefreshRequestSchema`, `TokenRefreshResponseSchema` to src/schemas/accounts.py

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -44,3 +44,22 @@ class PasswordResetCompleteRequestSchema(BaseModel):
     email: EmailStr
     token: str
     password: str
+
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
